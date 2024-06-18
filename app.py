113cb569ad793911ff9aab57444a824502afcdd0
Serkan Polat <itsonlydatahustle@gmail.com>
2024-06-19 00:28:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import numpy as np
 npNaN = np.nan
-import pandas_ta as pta
 import matplotlib.pyplot as plt
 import mpld3
 import streamlit.components.v1 as components
