0cbb4a115c5755efb531b7a4099134334f9dcbe4
Serkan Polat <itsonlydatahustle@gmail.com>
2024-06-19 01:24:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 # Importing numpy under alias np
 from numpy import NaN as npNaN
@@ -17,7 +18,6 @@ from tempfile import NamedTemporaryFile
 from yahoo_fin import stock_info as si
 from streamlit_option_menu import option_menu
 
-
 base="light"
 
 def main():
@@ -28,6 +28,8 @@ def main():
     # İletişim bilgilerini sidebar içinde görüntüledim
     st.sidebar.subheader("E-posta:")
     st.sidebar.write("itsonlydatahustle@gmail.com")
+    st.sidebar.subheader("Linkedln:")
+    st.sidebar.write("www.linkedin.com/in/serkann-polatt")
     st.sidebar.subheader("Github:")
     st.sidebar.write("https://github.com/serkannpolatt")
     st.sidebar.subheader("Kaggle:")
@@ -37,6 +39,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 figs=[]
@@ -49,12 +52,13 @@ Hisse senedi hareketini daha iyi anlamak için sadece birkaç teknik gösterge s
 # Kullanıcıdan hisse senedi simgesini al
 ticker = st.text_input("Hisse Senedi Göstergesi")
 
-# Eğer bir simge girilmemişse, varsayılan olarak "EREGL" olarak ayarladım
+ticker = ticker.upper()
+# Eğer bir simge girilmemişse, varsayılan olarak "BIST100" olarak ayarladım
 if ticker == "":
-    ticker = "EREGL.IS"
-
+    ticker = "XU100.IS"
 # Girilen simgeyi görüntüle
-st.write("Örnek Sembol Girişi:", ticker)
+st.write("**BIST100 için örnek sembol girişi:** **PGSUS.IS**, **DOAS.IS** **vb.**")
+st.write("**S&P500 için örnek sembol girişi:** **^GSPC**, **NVDA**, **TSLA**, **AMZN** **vb.**")
 
 # Finansal API'den (Örn: Yahoo Finance) simgeye ait hisse verilerini aldım
 df = si.get_data(ticker)
@@ -328,11 +332,6 @@ st.markdown("***")
 
 
 
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression
-import streamlit as st
-
 st.markdown("## Gelecek Fiyat Tahminleri")
 
 # Veri kümesini hazırla
@@ -427,15 +426,6 @@ predicted_prices = np.array(predicted_prices)
 tot_prices = np.reshape(tot_prices, (tot_prices.shape[0]))
 predicted_prices = np.reshape(predicted_prices, (predicted_prices.shape[0]))
 
-print(len(close_prices))
-print(len(tot_prices))
-
-
-
-
-
-
-
 fig = plt.figure()
 plt.plot(tot_prices, label='Tahmin Edilen Gelecek Fiyatlar')
 plt.plot(close_prices, label='Şimdiki fiyatlar')
@@ -446,7 +436,6 @@ plt.legend()
 fig_html = mpld3.fig_to_html(fig)
 components.html(fig_html, height=500)
 figs.append(fig)
-
 
 
 
@@ -679,3 +668,4 @@ if export_as_pdf:
     html = create_download_link(pdf.output(dest="S").encode("latin-1"), f"{ticker} analizi")
     st.markdown(html, unsafe_allow_html=True)
     st.text("")
+    
